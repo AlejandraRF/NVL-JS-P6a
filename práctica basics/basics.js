2c08b9a9d692023059684776e983c7c9f3545a5d
AlejandraRF <61287061+AlejandraRF@users.noreply.github.com>
2020-03-31 17:34:12 +0200

    Add files via upload

---
+++ práctica basics/basics.js
@@ -0,0 +1,6 @@
+var nombre = "Alejandra";
+var apellidos = "Regaldo Figueros"
+var nombre_a = nombre + "" + apellidos;
+var dias_semana=['Lunes', 'Martes','Miercoles','Jueves','Viernes','Sábado','Domigo']
+var gravedad= 9.8;
+var tres_primeros_impares=[1,3,5];
